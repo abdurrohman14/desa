400a5bf14171520abac43f4d77583e82580f6721
abdurrohman14 <rabd5428@gmail.com>
2025-06-24 10:57:27 +0700

    nonaktifkan dashboard

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,9 @@ use App\Http\Controllers\Warga\SuratPengajuanController;
 //     return view('partials.adminDashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
